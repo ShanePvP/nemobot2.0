a545be5b3b5a054518f7454b185d80f165e7329f
5tanly <31938727+5tanly@users.noreply.github.com>
2018-05-21 21:41:42 -0400

    Update owner_commands.py

--- plugins/owner_commands.py
+++ plugins/owner_commands.py
@@ -2,17 +2,6 @@
 from discord.ext import commands
 import plugins.json
 import os
-import asyncio
-
-async def cog_reload():
-    initial_extensions = plugins.json.read_json('plugins')
-    for extension in initial_extensions:
-        try:
-            self.bot.unload_extension(extension)
-            self.bot.load_extension(extension)
-            await ctx.send('Reloaded: ``'+extension+'``')
-        except:
-            await ctx.send('Failed: ``'+extension+'``')
 
 class OwnerCog:
 
@@ -21,15 +10,20 @@ class OwnerCog:
         
     @commands.command(name='reload')
     @commands.is_owner()
-    async def reload(self, ctx):
-        await cog_reload()
+    async def cog_reload(self, ctx):
+        initial_extensions = plugins.json.read_json('plugins')
+        for extension in initial_extensions:
+            try:
+                self.bot.unload_extension(extension)
+                self.bot.load_extension(extension)
+                await ctx.send('Reloaded: ``'+extension+'``')
+            except:
+                await ctx.send('Failed: ``'+extension+'``')
             
     @commands.command(name='update')
     @commands.is_owner()
     async def bot_update(self, ctx):
         os.system('cd && cd CCTV && git pull https://github.com/5tanly/CCTV')
-        await asyncio.sleep(2)
-        await cog_reload()
 
 def setup(bot):
     bot.add_cog(OwnerCog(bot))
