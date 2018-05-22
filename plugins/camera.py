1f0e2f17cdb258e43ec371c3332b9a9917305819
5tanly <31938727+5tanly@users.noreply.github.com>
2018-05-21 21:09:47 -0400

    Update camera.py

--- plugins/camera.py
+++ plugins/camera.py
@@ -12,9 +12,10 @@ class Camera:
     async def pic(self, ctx):
         path = os.getcwd()+"/camera/pic.jpg"
         os.system('raspistill -o os.getcwd()+"/camera/pic.jpg"')
-        embed=discord.Embed(color=0xff8000)
-        embed.add_field(name=datetime.datetime.now().strftime("%y-%m-%d"), value=datetime.datetime.now().strftime("%H:%M"), inline=False)
-        await ctx.send(discord.File=path)
+        f = discord.File(path, filename="image.png")
+        e = discord.Embed()
+        e.set_image(url="attachment://image.png")
+        await messagable.send(file=f, embed=e)
       
 def setup(bot):
     bot.add_cog(Camera(bot))
