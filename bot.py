6a41fe44a1a4c0dd354b85367ed54b571e7710be
5tanly <31938727+5tanly@users.noreply.github.com>
2018-05-21 21:23:55 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,27 +9,11 @@ import discord
 from discord.ext import commands
 import asyncio
 import os
-#import json
 import plugins.json
-'''
-config_file = json.load(open(os.getcwd()+'/bot_config/config.json'))
-'''
+
 config_file = plugins.json.read_json('config')
 prefix = config_file['prefix']
 token = config_file['token']
-
-'''
-with open('bot_config/token.txt', 'r') as token_file:
-    token = token_file.read()
-'''
-
-'''
-def get_prefix(bot, message):
-    if not message.guild:
-        return '?'
-    return commands.when_mentioned_or('!')(bot, message)
-'''
-
 
 initial_extensions = plugins.json.read_json('plugins')
 
@@ -45,11 +29,11 @@ async def on_ready():
 if __name__ == '__main__':
     print('------')
     for extension in initial_extensions:
-        #try:
-        bot.load_extension(extension)
-            #print('EXTENSION LOADED SUCCESSFULLY: '+extension)
-        #except:
-            #print('EXTENSION FAILEDF TO LOAD: '+extension)
+        try:
+            bot.load_extension(extension)
+            print('EXTENSION LOADED SUCCESSFULLY: '+extension)
+        except:
+            print('EXTENSION FAILEDF TO LOAD: '+extension)
             
 
 bot.run(token)
