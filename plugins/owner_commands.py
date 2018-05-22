4a3807addcf95de07173022768420a93c582f3ac
5tanly <31938727+5tanly@users.noreply.github.com>
2018-05-21 21:20:09 -0400

    Update owner_commands.py

--- plugins/owner_commands.py
+++ plugins/owner_commands.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
 import plugins.json
+import os
 
 class OwnerCog:
 
@@ -9,9 +10,7 @@ class OwnerCog:
     @commands.command(name='reload')
     @commands.is_owner()
     async def cog_reload(self, ctx):
-
         initial_extensions = plugins.json.read_json('plugins')
-        
         for extension in initial_extensions:
             try:
                 self.bot.unload_extension(extension)
@@ -19,7 +18,11 @@ class OwnerCog:
                 await ctx.send('Reloaded: ``'+extension+'``')
             except:
                 pass
-
+            
+    @commands.command(name='update')
+    @commands.is_owner()
+    async def bot_update(self, ctx):
+    os.system('cd && cd os.getcwd() && git pull https://github.com/5tanly/CCTV')
 
 def setup(bot):
     bot.add_cog(OwnerCog(bot))
