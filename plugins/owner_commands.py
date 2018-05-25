c337b371d3bc027ffa92b43b82382bbd0e80f9ab
5tanly <31938727+5tanly@users.noreply.github.com>
2018-05-25 00:16:18 -0400

    Update owner_commands.py

--- plugins/owner_commands.py
+++ plugins/owner_commands.py
@@ -19,14 +19,37 @@ class OwnerCog:
                 await ctx.send(':white_check_mark: Reloaded: ``'+extension+'``')
             except:
                 await ctx.send(':x: Failed: ``'+extension+'``')
+                
+    @commands.command(name='reboot')
+    @commands.is_owner()
+    async def pi_reboot(self, ctx):
+        await ctx.send('Rebooting')
+        os.system('sudo reboot')
+        
+    @commands.command(name='shutdown')
+    @commands.is_owner()
+    async def pi_shutdown(self, ctx):
+        await ctx.send('Shutting Down')
+        os.system('sudo shutdown now')
             
     @commands.command(name='update')
     @commands.is_owner()
     async def bot_update(self, ctx):
+        ##PULL FROM GITHUB
         config_file = plugins.json.read_json('config')
         prefix = config_file['prefix']
         os.system('cd && cd ' + os.getcwd() + ' && git pull https://github.com/5tanly/CCTV')
         await ctx.send('Updated!\nBe sure to reload the plugins by typing ``'+prefix+'reload``')
+        
+        ##RELOAD PLUGINS
+        initial_extensions = plugins.json.read_json('plugins')
+        for extension in initial_extensions:
+            try:
+                self.bot.unload_extension(extension)
+                self.bot.load_extension(extension)
+                await ctx.send(':white_check_mark: Reloaded: ``'+extension+'``')
+            except:
+                await ctx.send(':x: Failed: ``'+extension+'``')
 
 def setup(bot):
     bot.add_cog(OwnerCog(bot))
